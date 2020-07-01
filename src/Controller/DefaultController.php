618beccabaf153ace3a9bd53812a73461e128c78
Hassen Ben Slama <bensalma@vorwaerts.com>
2020-07-01 20:47:46 +0200

    Add home route and controller function

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,11 +3,20 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController {
     public function index()
     {
         return $this->render('index.html.twig');
+    }
+
+    /**
+     * @Route("/home", name="home")
+     */
+    public function home()
+    {
+        return $this->render('home.html.twig');
     }
 
 }
