99bea9f591dca04374dea9c0e6feb0746f65af12
Hassen Ben Slama <bensalma@vorwaerts.com>
2020-07-02 16:13:32 +0200

    Handle Exceptions for User creation

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,33 +6,43 @@ use App\Entity\User;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class  UserController extends AbstractController
 {
     /**
-     * @Route("/user/create", name="create_user", methods={"POST"})
+     * @Route("/signup", name="create_user", methods={"POST"})
      * @param Request $request
      * @param UserPasswordEncoderInterface $encoder
-     * @return RedirectResponse
+     * @return Response
      */
     public function create(Request $request, UserPasswordEncoderInterface $encoder)
     {
         $entityManager = $this->getDoctrine()->getManager();
-
         $username = trim($request->request->get('username'));
         $email = trim($request->request->get('email'));
         $password = trim($request->request->get('password'));
 
-        $user = new User();
-        $user->setUserName($username);
-        $user->setEmail($email);
-        $user->setPassword($encoder->encodePassword($user, $password));
+        try {
+            $user = new User();
+            $user->setUserName($username);
+            $user->setEmail($email);
+            $user->setPassword($encoder->encodePassword($user, $password));
 
-        $entityManager->persist($user);
-        $entityManager->flush();
+            $entityManager->persist($user);
+            $entityManager->flush();
 
-        return $this->redirectToRoute('app_login');
+            return $this->render('security/login.html.twig',[
+                'message' => 'Account Creation Successful! Please Login'
+            ]);
+        } catch(\Exception $e){
+            $error = $e->getMessage();
+            var_dump($error);
+            return $this->render('security/login.html.twig',[
+                'error' => 'Account Creation Failed!'
+            ]);
+        }
     }
 }
