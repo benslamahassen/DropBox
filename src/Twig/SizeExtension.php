b0ca46ee7952f73ca7b76290b5e40d4260812dea
Hassen Ben Slama <bensalma@vorwaerts.com>
2020-07-02 18:09:15 +0200

    File Size converter Extension from Byte to KB,MB etc..

---
+++ src/Twig/SizeExtension.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Twig;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class SizeExtension extends AbstractExtension
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+
+    /**
+     * Constructor
+     *
+     * @param ContainerInterface $container
+     */
+    public function __construct(
+        ContainerInterface $container
+    )
+    {
+        $this->container = $container;
+    }
+
+    public function getFilters()
+    {
+        return array(
+            new TwigFilter('formatBytes', array($this, 'formatBytes')),
+        );
+    }
+
+    /**
+     * @param $bytes
+     * @param int $precision
+     * @return string
+     */
+    public function formatBytes($bytes, $precision = 2)
+    {
+        $size = ['B','KB','MB','GB','TB','PB','EB','ZB','YB'];
+        $factor = floor((strlen($bytes) - 1) / 3);
+        return sprintf("%.{$precision}f", $bytes / pow(1024, $factor)) . @$size[$factor];
+    }
+}
